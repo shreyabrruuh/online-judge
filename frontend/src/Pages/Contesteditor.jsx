2f013e57e4739618b2e543dcfe3b18d10bfa7cd5
shreyabrruuh <145364703+shreyabrruuh@users.noreply.github.com>
2025-01-26 14:42:42 +0530

    Update Contesteditor.jsx

--- frontend/src/Pages/Contesteditor.jsx
+++ frontend/src/Pages/Contesteditor.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Editor } from '@monaco-editor/react';
 import axios from 'axios';
-import { useParams , Link} from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Lang_selector from '../Components/Lang_selector.jsx';
 import DOMPurify from 'dompurify';
 import { useAuth } from '../AuthContext.jsx';
@@ -18,7 +18,7 @@ const Contesteditor = () => {
       // Return 0 to indicate successful execution
       return 0; 
   }`);
-  const {role, user} = useAuth();
+  const { role, user } = useAuth();
   const [language, setLanguage] = useState('cpp');
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
@@ -58,7 +58,7 @@ const Contesteditor = () => {
   useEffect(() => {
     const fetchCode = async () => {
       try {
-        const response = await axios.get(`https://online-judge-qmoq.onrender.com/code/get/${id}`);
+        const response = await axios.get(`http://localhost:8000/code/get/${id}`);
         if (response.data.code) {
           setCode(response.data.code);
         } else {
@@ -74,7 +74,7 @@ const Contesteditor = () => {
         if (!id) {
           throw new Error('Problem ID is undefined');
         }
-        const response = await axios.get(`https://online-judge-qmoq.onrender.com/allContestProblembyId/${id}`);
+        const response = await axios.get(`http://localhost:8000/allContestProblembyId/${id}`);
         const problemData = response.data;
         setProblem(problemData);
         setTestCases(response.data.testCases);
@@ -97,7 +97,7 @@ const Contesteditor = () => {
   useEffect(() => {
     const saveCode = async () => {
       try {
-        await axios.post('https://online-judge-qmoq.onrender.com/code/save', {
+        await axios.post('http://localhost:8000/code/save', {
           id,
           code
         });
@@ -124,7 +124,7 @@ const Contesteditor = () => {
     };
 
     try {
-      const { data } = await axios.post(` https://compiler.codeuniverse.tech/run`, payload);
+      const { data } = await axios.post(`http://localhost:8080/run`, payload);
       console.log(data);
       setOutput(data.output1);
     } catch (error) {
@@ -132,7 +132,7 @@ const Contesteditor = () => {
     }
   };
 
-  const handleSubmission = async ()=>{
+  const handleSubmission = async () => {
     const payload = {
       userId: user, // Assuming user object has an id field
       problemId: id,
@@ -143,13 +143,13 @@ const Contesteditor = () => {
       count: submissionCount
     };
     try {
-      const { data } = await axios.post(`https://online-judge-qmoq.onrender.com/submission`, payload);
-      console.log("Count:",data.count);
+      const { data } = await axios.post(`http://localhost:8000/submission`, payload);
+      console.log("Count:", data.count);
       setSubmissionCount(prevCount => prevCount + 1);
     } catch (error) {
-      console.log("Error fetching submissionCount:",error.response);
+      console.log("Error fetching submissionCount:", error.response);
     }
-  }
+  };
 
   const handleSubmit = async () => {
     console.log('handleSubmit called');
@@ -162,7 +162,7 @@ const Contesteditor = () => {
     };
 
     try {
-      const { data } = await axios.post(`https://online-judge-qmoq.onrender.com/Contestsubmit`, payload);
+      const { data } = await axios.post(`http://localhost:8000/Contestsubmit`, payload);
       console.log(data);
       setVerdict(data.verdict);
       setTestCaseResults(data.results); // Set test case results
@@ -173,7 +173,7 @@ const Contesteditor = () => {
   };
   const handleDelete = async () => {
     try {
-      await axios.delete(`https://online-judge-qmoq.onrender.com/problem/${id}`);
+      await axios.delete(`http://localhost:8000/problem/${id}`);
       alert('Problem deleted successfully');
       navigate('/contest'); // Redirect to problems page after deletion
     } catch (error) {
@@ -188,7 +188,7 @@ const Contesteditor = () => {
         d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM13.6695 15.9999H10.3295L8.95053 17.8969L9.5044 19.6031C10.2897 19.8607 11.1286 20 12 20C12.8714 20 13.7103 19.8607 14.4956 19.6031L15.0485 17.8969L13.6695 15.9999ZM5.29354 10.8719L4.00222 11.8095L4 12C4 13.7297 4.54894 15.3312 5.4821 16.6397L7.39254 16.6399L8.71453 14.8199L7.68654 11.6499L5.29354 10.8719ZM18.7055 10.8719L16.3125 11.6499L15.2845 14.8199L16.6065 16.6399L18.5179 16.6397C19.4511 15.3312 20 13.7297 20 12L19.997 11.81L18.7055 10.8719ZM12 9.536L9.656 11.238L10.552 14H13.447L14.343 11.238L12 9.536ZM14.2914 4.33299L12.9995 5.27293V7.78993L15.6935 9.74693L17.9325 9.01993L18.4867 7.3168C17.467 5.90685 15.9988 4.84254 14.2914 4.33299ZM9.70757 4.33329C8.00021 4.84307 6.53216 5.90762 5.51261 7.31778L6.06653 9.01993L8.30554 9.74693L10.9995 7.78993V5.27293L9.70757 4.33329Z">
       </path>
     </svg> ading . . .</h1>
-  </div></div>
+  </div></div>;
   if (error) return <div>Error: {error}</div>;
 
   if (!problem) return <div>No problem data available.</div>;
@@ -261,7 +261,7 @@ const Contesteditor = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Contesteditor;
